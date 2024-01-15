8626ff9107dce43fdbad399af1ad783c09c8ab68
Stuti-Rai <stutirai2060@gmail.com>
2024-01-15 02:04:24 +0000

    initial testFile commit

--- CW1_tests.cpp
+++ CW1_tests.cpp
@@ -1,16 +1,18 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "CW1.cpp"// Include the header file where your library code is defined
 
-TEST_CASE("Example Test Case", "[example]") {
-    // Your test code goes here
-
-    // Example: Check if addition works
-    REQUIRE(1 + 1 == 2);
+TEST_CASE("Library System Tests", "[library]") {
+     Librarian admin(1, "Name", "Adress", "email@mail.com", 00);
+     REQUIRE(admin.getStaffId()==1);
+     REQUIRE(admin.getSalary()==0);
 }
 
-TEST_CASE("Another Test Case", "[example]") {
-    // Another test code goes here
-
-    // Example: Check if subtraction works
-    REQUIRE(3 - 2 == 1);
+TEST_CASE("Library System Test2", "[library]") {
+    Person person("Name", "Adress", "email@mail.com");
+     REQUIRE(person.getName()=="Name");
+     REQUIRE(person.getAddress()=="Adress");
+    REQUIRE(person.getEmail()=="email");
 }
+
+
